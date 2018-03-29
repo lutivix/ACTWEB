e74cabf0d2fd84c12411b230ca35e2ac6c0b6e69
pedro <pedro@lfsolutions.net.br>
2018-03-29 14:16:56 -0300

    SLF405 - Inserido DAO e Entidades

---
+++ branches/LFSistemas.VLI.ACTWeb.Entities/Estacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFSistemas.VLI.ACTWeb.Entities
+{
+    public class Estacao
+    {
+        public double id;
+        public string sigla;
+        public string nome;
+    }
+}
