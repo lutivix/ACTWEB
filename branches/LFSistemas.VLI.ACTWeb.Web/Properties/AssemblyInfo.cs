043eed599295c317c521966e1196bc52d4996da6
lutivix <luti.vix@gmail.com>
2021-04-26 09:42:12 -0300

    Luciano - C859 - Consultas Estacao de Landmark (compilação)
    	ACTWEB 30.714.859.67

--- branches/LFSistemas.VLI.ACTWeb.Web/Properties/AssemblyInfo.cs
+++ branches/LFSistemas.VLI.ACTWeb.Web/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ACTWEB WEB - C875")]
-[assembly: AssemblyDescription("SLF179 - Filtros THP Subrotas")]
+[assembly: AssemblyTitle("ACTWEB WEB - C859")]
+[assembly: AssemblyDescription("C859 - Landmark Estacao")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LF Solutions")]
 [assembly: AssemblyProduct("LFSistemas.VLI.ACTWeb.Web")]
